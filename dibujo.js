e41e4185cf1abda75f688f2577c9c95f2b852836
John Ramirez <jjramirez3@misena.edu.co>
2021-09-15 22:11:11 -0500

    caracteristicas del js

--- dibujo.js
+++ dibujo.js
@@ -1,3 +1,10 @@
 var d = document.getElementById ("mi_dibujo");
 var lienzo = d.getContext("2d");
-console.log(lienzo);
+
+lienzo.beginPath();
+lienzo.fillStyle = '#F0DB4F';
+lienzo.strokeStyle = "Red";
+lienzo.moveTo(50, 50, 150, 100);
+lienzo.lineTo(200, 200);
+lienzo.stroke();
+lienzo.closePath();
